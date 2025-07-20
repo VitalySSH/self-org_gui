620fc8bb1523713ccde68fdcd98f31379c6c640a
Vitaly Sharonov <SharonovVS@mosreg.ru>
2025-07-20 19:28:19 +0300

    Fix baseApiUrl.

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,3 @@
-export const baseApiUrl = 'http://localhost:8080/api/v1';
-// export const baseApiUrl = 'http://89.169.184.5/api/';
+// export const baseApiUrl = 'http://localhost:8080/api/v1';
+export const baseApiUrl = 'http://89.169.184.5/api/';
 export const AOApiUrl = `${baseApiUrl}/ao`;
